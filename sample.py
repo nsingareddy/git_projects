5c32938214ee5885fbdb1ec810e9e1fffd003e0f
nsingareddy <neelima.singareddy@gmail.com>
2018-12-12 12:24:02 -0500

    added dictionary comprehension function

--- sample.py
+++ sample.py
@@ -1,3 +1,9 @@
 alist = [lambda m:m**2, lambda m,n:m*n, lambda m:m**4]
 
 print(alist[0](10), alist[1](2, 20), alist[2](3))
+
+key = 'm'
+
+aDict = {'m': lambda x:2*x, 'n': lambda x:3*x}
+
+print(aDict[key](9)) # Output: 18
